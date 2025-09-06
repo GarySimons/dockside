05b3be4d7d7bef905757702e9179348eab99b2aa
GarySimons <garycsimons@gmail.com>
2025-09-06 11:19:58 +0100

    Intial commit

---
+++ src/routes/Home.js
@@ -0,0 +1,16 @@
+import React from "react";
+
+const Home = () => {
+  return (
+    <div
+      style={{
+        background: "pink",
+        height: "100vh",
+      }}
+    >
+      Home
+    </div>
+  );
+};
+
+export default Home;
