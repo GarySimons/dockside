c4d835238df44e96233f1bb14f77f6450f1e4785
GarySimons <garycsimons@gmail.com>
2025-09-13 18:28:09 +0100

    padding removed from CustomTabPanel

--- src/routes/Menus.js
+++ src/routes/Menus.js
@@ -29,7 +29,7 @@ function CustomTabPanel(props) {
       aria-labelledby={`simple-tab-${index}`}
       {...other}
     >
-      {value === index && <Box sx={{ p: 3 }}>{children}</Box>}
+      {value === index && <Box sx={{ p: 0 }}>{children}</Box>}
     </div>
   );
 }
@@ -178,7 +178,7 @@ const Menus = () => {
             </CustomTabPanel>
           </Box>
           <Box sx={{ width: "100%" }}>
-            <Box sx={{ borderBottom: 1, borderColor: "divider" }}>
+            <Box sx={{}}>
               <Box
                 sx={{
                   display: "flex",
